aeb04cee7f0d673f0ccf51f85326a3b9de1af3bf
Morgan <57332487+malonzo143@users.noreply.github.com>
2020-12-28 20:36:22 -0600

    React Week 1 Assignment

---
+++ ReactWeek1Assignment.js
@@ -0,0 +1,27 @@
+class Student {
+constructor(name,email,community){
+  this.name = "name";
+  this.email = "email";
+  this.community = "community";
+}
+};
+
+
+class Bootcamp {
+  constructor (name,level, students=[]){
+this.name = "name";
+this.level ="level";
+this.students = "students"
+  }
+  registerStudent(student){
+
+    if(this.students.filter(s =>s.email === student.email).length){
+      console.log(`This ${student.email} is already registered for Web Dev Fundamentals!`);
+    }else{
+      this.students.push(student);
+      console.log(`Registering ${student.email} to the bootcamp ${this.name}`);
+    }
+    return this.students;
+  }
+};
+
